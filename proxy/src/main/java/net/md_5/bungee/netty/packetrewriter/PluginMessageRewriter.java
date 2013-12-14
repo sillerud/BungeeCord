c5e701d7d3e0b6c3b5d63f6a89db3fd4f048fa9c
Howaner <franz.moos@googlemail.com>
2013-12-14 23:34:10 +0100

    Remove old imports

--- proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/PluginMessageRewriter.java
+++ proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/PluginMessageRewriter.java
@@ -2,17 +2,15 @@ package net.md_5.bungee.netty.packetrewriter;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import net.md_5.bungee.netty.Var;
 
 public class PluginMessageRewriter extends PacketRewriter
 {
 
-    @Override
-    public void rewriteClientToServer(ByteBuf in, ByteBuf out)
-    {
-        String channel = Var.readString( in, true );
+	@Override
+	public void rewriteClientToServer(ByteBuf in, ByteBuf out)
+	{
+		String channel = Var.readString( in, true );
 		short length = in.readShort();
 		ByteBuf bytes = in.readBytes( length );
 		
@@ -31,34 +29,34 @@ public class PluginMessageRewriter extends PacketRewriter
 		}
 		
 		Var.writeString( channel, out, false );
-        out.writeShort( length );
-        out.writeBytes( bytes );
-    }
+		out.writeShort( length );
+		out.writeBytes( bytes );
+	}
 
-    @Override
-    public void rewriteServerToClient(ByteBuf in, ByteBuf out)
-    {
-        String channel = Var.readString( in, false );
-        short length = in.readShort();
-        ByteBuf bytes = in.readBytes( length ); //OLD: in.readableBytes()
-        
-        // Command Block Rewrite
-        if ( channel.equals( "MC|AdvCdm" ) ) {
-        	ByteBuf buf = Unpooled.buffer();
-        	
-        	// x - int, y - int, z - int
-        	// 4(int) + 4(int) + 4(int) = 12
-        	buf.writeByte( 0 );
-        	buf.writeBytes( bytes.readBytes( 12 ) );
+	@Override
+	public void rewriteServerToClient(ByteBuf in, ByteBuf out)
+	{
+		String channel = Var.readString( in, false );
+		short length = in.readShort();
+		ByteBuf bytes = in.readBytes( length );
+		
+		// Command Block Rewrite
+		if ( channel.equals( "MC|AdvCdm" ) ) {
+			ByteBuf buf = Unpooled.buffer();
+			
+			// x - int, y - int, z - int
+			// 4(int) + 4(int) + 4(int) = 12
+			buf.writeByte( 0 );
+			buf.writeBytes( bytes.readBytes( 12 ) );
 			Var.writeString( Var.readString( bytes, false ), buf, true );
 			
 			bytes = buf;
 			length = (short) bytes.readableBytes();
-        }
-        
-        Var.writeString( channel, out, true );
-        out.writeShort( length );
-        out.writeBytes( bytes );
-    }
+		}
+		
+		Var.writeString( channel, out, true );
+		out.writeShort( length );
+		out.writeBytes( bytes );
+	}
 
 }
